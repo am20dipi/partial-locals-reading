c9591f868a0c542fa5f9def3db3bd57b44d811ad
Adriana <am20dipi@alum.siena.edu>
2021-04-01 11:56:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @author = @post.author
+    #@author = @post.author -- we do NOT need this because of the locals hash
   end
 
   def index
